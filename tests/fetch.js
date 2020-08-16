52aad2bbf195ecae23c1ff281f27a8b5ee7b0313
Nathan Blair <engineer.nblair@gmail.com>
2020-08-16 11:19:53 -0400

    now using timeouts for http/2 calls

--- tests/fetch.js
+++ tests/fetch.js
@@ -3,26 +3,51 @@ import { strict as assert } from "assert"
 import { fetch } from "../lib/net/http2_client.js"
 
 export const id = "Test network client"
+const timeout = 1000
 
 export const assertions = {
   "HTTP/2 Fetch test site": {
     function: async () => {
-      const test_site_content = await fetch(
-        "https://sonicoriginalsoftware.github.io/test-site/",
-      )
+      try {
+        var test_site_content = await fetch(
+          "https://sonicoriginalsoftware.github.io/test-site/",
+          { timeout: timeout }
+        )
+      } catch (err) {
+        assert.fail(err)
+      }
       const expected_content = "<html>\n<body>\nTest site!\n</body>\n</html>\n"
       assert.deepStrictEqual(test_site_content[1], expected_content)
     },
-    skip: false
+  },
+  "HTTP/2 Check timeout": {
+    function: async () => {
+      const start_time = process.hrtime()
+      try {
+        await fetch("https://sonicoriginalsoftware.github.io/test-site/", {
+          timeout: timeout,
+        })
+      } catch (err) {
+        assert.fail(err)
+      }
+      const [, duration_nanoseconds] = process.hrtime(start_time)
+      const duration = duration_nanoseconds / 1000000
+      assert.deepStrictEqual(
+        duration <= timeout + 10,
+        true,
+        `${duration} > ${timeout}`
+      )
+    },
   },
   "HTTPS Fetch test site": {
     function: async () => {
       const test_site_content = await fetch(
         "https://sonicoriginalsoftware.github.io/test-site/",
+        { timeout: timeout }
       )
       const expected_content = "<html>\n<body>\nTest site!\n</body>\n</html>\n"
       assert.deepStrictEqual(test_site_content[1], expected_content)
     },
-    skip: true
+    skip: true,
   },
 }
