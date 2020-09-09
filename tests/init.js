e1ed807f5f2fa39dec14c726eb24b915ab8c8d90
Nathan Blair <engineer.nblair@gmail.com>
2020-09-09 11:41:36 -0400

    removing test-data directory before init tests

--- tests/init.js
+++ tests/init.js
@@ -1,10 +1,13 @@
 import { strict as assert } from "assert"
 import { PassThrough } from "stream"
 import { Console } from "console"
+import { rmdir } from "fs/promises"
 
 import { init, usage } from "../lib/commands/init.js"
 
 export const id = "Test init"
+
+const test_dir = "./test-data"
 
 export const assertions = {
   "Show Initialize help": {
@@ -17,7 +20,10 @@ export const assertions = {
       const stderr_stream = new PassThrough()
       stderr_stream.on("data", (chunk) => (stderr += chunk))
 
-      const logger = new Console({ stdout: stdout_stream, stderr: stderr_stream })
+      const logger = new Console({
+        stdout: stdout_stream,
+        stderr: stderr_stream,
+      })
 
       assert.doesNotReject(() => init(["help"], logger))
       assert.deepStrictEqual(stderr.trim(), "")
@@ -27,8 +33,10 @@ export const assertions = {
   },
   "Default Initialize": {
     function: async () => {
+      await rmdir(test_dir, { recursive: true })
+
       try {
-        await init(["-t", "./test-data", "-u", "./test-data/server"], console)
+        await init(["-t", test_dir, "-u", test_dir + "/server"], console)
       } catch (err) {
         return assert.fail(err)
       }
