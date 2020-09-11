43cdf19e9f0abb17137d69499dbe8ac706ac0096
Nathan Blair <engineer.nblair@gmail.com>
2020-09-11 09:22:01 -0400

    getting finished with the add-component command

--- tests/add_component.js
+++ tests/add_component.js
@@ -4,15 +4,15 @@ import { Console } from "console"
 import { rmdir } from "fs/promises"
 import { join } from "path"
 
-import { init, usage } from "../lib/commands/init.js"
+import { add_component, usage } from "../lib/commands/add_component.js"
 import { TEST_DATA } from "./test_data.js"
 
 export const id = "Test init"
 
-const init_dir = join(TEST_DATA, "init")
+const add_component_dir = join(TEST_DATA, "add-component")
 
 export const assertions = {
-  "Show Initialize help": {
+  "Show Add-Component help": {
     function: () => {
       let stdout = ""
       let stderr = ""
@@ -27,23 +27,23 @@ export const assertions = {
         stderr: stderr_stream,
       })
 
-      assert.doesNotReject(() => init(["help"], logger))
+      assert.doesNotReject(() => add_component(["help"], logger))
       assert.deepStrictEqual(stderr.trim(), "")
       assert.deepStrictEqual(stdout.trim(), usage.trim())
     },
     skip: false,
   },
-  "Default Initialize": {
+  "Add test toast-lane component": {
     function: async () => {
-      await rmdir(init_dir, { recursive: true })
+      await rmdir(add_component_dir, { recursive: true })
 
       try {
-        await init(["-t", init_dir, "-u", init_dir + "/server"], console)
+        await add_component(["-n", "toast-lane", "-p", add_component_dir], console)
       } catch (err) {
         return assert.fail(err)
       }
       return assert.ok(true)
     },
-    skip: true,
+    skip: false,
   },
 }
