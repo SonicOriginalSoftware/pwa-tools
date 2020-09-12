55aa474a93cee94c5faae9b738e6db569e4705f4
Nathan Blair <engineer.nblair@gmail.com>
2020-09-12 00:26:24 -0400

    initial work done on #10

--- tests/add_resource.js
+++ tests/add_resource.js
@@ -4,7 +4,7 @@ import { Console } from "console"
 import { rmdir } from "fs/promises"
 import { join } from "path"
 
-import { add_component, usage } from "../lib/commands/add_component.js"
+import { add_resource, usage } from "../lib/commands/add_resource.js"
 import { TEST_DATA } from "./test_data.js"
 
 export const id = "Test add component"
@@ -27,7 +27,7 @@ export const assertions = {
         stderr: stderr_stream,
       })
 
-      assert.doesNotReject(() => add_component(["help"], logger))
+      assert.doesNotReject(() => add_resource(["help"], logger))
       assert.deepStrictEqual(stderr.trim(), "")
       assert.deepStrictEqual(stdout.trim(), usage.trim())
     },
@@ -38,7 +38,7 @@ export const assertions = {
       await rmdir(add_component_dir, { recursive: true })
 
       try {
-        await add_component(["-n", "toast-lane", "-p", add_component_dir], console)
+        await add_resource(["-n", "toast-lane", "-p", add_component_dir], console)
       } catch (err) {
         return assert.fail(err)
       }
